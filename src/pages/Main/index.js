79b33c467696b122e70295bd965b0145f135a035
DESKTOP-GOK3KSC\DELL <natalia.nideck@gmail.com>
2025-07-01 21:33:51 -0400

    Requesting API using Axios | animating loading button

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,64 @@
-import React from "react";
-import { FaGithub, FaPlus } from "react-icons/fa";
+import React, { useState, useCallback } from "react";
+import { FaGithub, FaPlus, FaSpinner } from "react-icons/fa";
 import { Container, Form, SubmitButton } from "./styles";
+import api from "../../services/api";
 
 export default function Main() {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositorios, setRepositorios] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = useCallback(
+    (e) => {
+      //*uso o useCallback para guardar deixar a função pronta e não ter que
+      //*carregar toda vez que o newRepo e repositorios muda
+      e.preventDefault();
+      async function submit() {
+        setLoading(true);
+        try {
+          const response = await api.get(`repos/${newRepo}`);
+          //*console.log(response.data);
+          const data = {
+            name: response.data.full_name,
+          };
+          setRepositorios([...repositorios, data]);
+          setNewRepo("");
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+      }
+      submit();
+    },
+    [newRepo, repositorios]
+  );
+
+  function handleInputChange(e) {
+    setNewRepo(e.target.value);
+  }
+
   return (
     <Container>
-    
       <h1>
-        <FaGithub size={25}/>
-        Meus Repositórios</h1>
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar Repositorios"></input>
+        <FaGithub size={25} />
+        Meus Repositórios
+      </h1>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar Repositorios"
+          value={newRepo}
+          onChange={handleInputChange}
+        ></input>
 
-        <SubmitButton>
-            <FaPlus color="#FFF" size={14} />
+        {
+          //{aqui estou passando um parametro usando o loading do useState
+        }
+        <SubmitButton loading={loading ? 1 : 0}>
+          {loading ? (
+            <FaSpinner color="#FFF" size={14} />
+          ) : (<FaPlus color="#FFF" size={14} />)}
         </SubmitButton>
       </Form>
     </Container>
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,50 +1,72 @@
-import { styled } from 'styled-components'
+import { styled, keyframes, css } from "styled-components";
 
 export const Container = styled.div`
- max-width: 700px;
- background-color: #FFF;
- border-radius: 4px;
- box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
- padding: 30px;
- margin: 80px auto;
- 
- h1 {
+  max-width: 700px;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  padding: 30px;
+  margin: 80px auto;
+
+  h1 {
     font-size: 20px;
     display: flex;
     flex-direction: row;
     align-items: center;
 
     svg {
-        margin-right: 10px;
-
+      margin-right: 10px;
     }
-
- }
+  }
 `;
 
 export const Form = styled.form`
-margin-top: 30px;
-display: flex;
-flex-direction: row;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
 
-input {
+  input {
     flex: 1;
     border: 1px solid #ddd;
     padding: 10px 15px;
     border-radius: 4px;
+  }
+`;
+
+//Criando animação do botão
+const animate = keyframes`
+from {
+    transform: rotate(0deg);
+} to {
+    transform: rotate(360deg);
 }
 `;
 
-export const SubmitButton = styled.button`
-background-color: #0d2636;
-border: 0;
-border-radius: 4px;
-margin-left: 10px;
-padding: 0 15px;
-display: flex;
-align-items: center;
-justify-content: center;
-;
+//*.attrs serve para adicionar atributos ao componente
+export const SubmitButton = styled.button.attrs((props) => ({
+  type: "submit",
+  disabled: props.loading,
+}))`
+  background-color: #0d2636;
+  border: 0;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
+  //*para estilizar o componente a partir da propriedade capturada:
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
 
+  ${(props) =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${animate} 2s linear infinite;
+      }
+    `}
 `;
